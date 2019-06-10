1e8426a66dd7b0ccbcedb718e3240f6a80bedf5d
Candy Wang <candywang013@gmail.com>
2019-06-10 11:43:07 -0700

    passing all tests for ES6

--- src/Bee.js
+++ src/Bee.js
@@ -1,8 +1,8 @@
 class Bee extends Grub {
   constructor(age, color, job) {
-    super (age, color, job);
+    super(age, color, job);
     this.age = 5;
     this.color = 'yellow';
     this.job = 'Keep on growing';
-  }
+  };
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,12 +1,12 @@
 class Grub {
-    constructor () {
+    constructor() {
         this.age = 0;
         this.color = 'pink';
         this.food = 'jelly';
-        }
+        };
 
     eat() {
         return 'Mmmmmmmmm jelly';
-        }; 
+        };
 };
 
